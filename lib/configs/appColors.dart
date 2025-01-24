57d1f046ea05c5f19a8ad265f8a987627fecd2fa
Gul524 <gullsuleman524@gmail.com>
2025-01-24 23:30:55 +0500

    2nd day ,, clor are setted

---
+++ lib/configs/appColors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class lightcolors {
+  static Color bg = Colors.white;
+  static Color Container = const Color.fromARGB(255, 138, 134, 134);
+  static Color primary = Colors.blue;
+  static Color font = const Color.fromARGB(255, 0, 0, 0);
+  static Color secondary = Colors.amber;
+
+}
+
+class darkcolors {
+  static Color bg = const Color.fromARGB(255, 1, 21, 65);
+  static Color Container = const Color.fromARGB(255, 84, 52, 209);
+  static Color primary = Colors.blue;
+  static Color font = Colors.white;
+  static Color secondary = Colors.amber;
+
+}
